8000929a7673dc30b353da2bdda47171c0119e77
loma <271341974@qq.com>
2018-01-15 18:15:38 +0800

    first init

--- app/src/main/java/com/example/loma/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/loma/myapplication/MainActivity.kt
@@ -15,32 +15,19 @@ import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var adapter:  RecyclerViewAdapter<String>
+
     var i = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-        fab.setOnClickListener {
-            adapter.add("${i++}")
-        }
 
 
-//        list_view.setEmptyView(getLayoutInflater().inflate(R.layout.empty, null))
-        adapter = object : RecyclerViewAdapter<String>(android.R.layout.simple_list_item_1) {
-            override fun onBind(holder: BaseViewHolder, item: String, position: Int) {
-
-                holder.setText(android.R.id.text1, item)
 
 
-            }
 
 
-        }
-
-
-        adapter.itemClickDoing {  p -> adapter.remove(p) }
 
 
         doAsync {
@@ -54,18 +41,7 @@ class MainActivity : AppCompatActivity() {
 
         val list = ArrayList<String>()
 
-        adapter.data = list
-        list_view.adapter = adapter
-//        val recyclerOnScrollListener = object : RecyclerOnScrollListener() {
-//          override  fun onLoadNextPage(view: View) {
-//                super.onLoadNextPage(view)
-//                toast("ddddddddddddddddddddddddd")
-//              adapter.add("${i++}")
-//            }
-//        }
-//
-//
-//        list_view.addOnScrollListener(recyclerOnScrollListener)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
